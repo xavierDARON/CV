1fa0b808ecf8618b6540f1dc5a3dfb9a1722d3de
Xavier DARON <xavier_daron@orange.fr>
2019-04-25 16:53:50 +0200

    initialisation

---
+++ assets/js/monjs.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+//   $("#hide").click(function(){
+//     $("#hide01").hide();
+//   });
+//   $("#show").click(function(){
+//     $("#hide01").show();
+//   });
+// });
+
+$(document).ready(function(){
+  $(".show01").click(function(){
+    $(".hide01").toggle();
+  });
+  $(".show02").click(function(){
+    $(".hide02").toggle();
+  });
+});
