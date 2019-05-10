dd5dbd8ac81f8ca33b9fd3c24bce56ffec58c85b
Xavier DARON <xavier_daron@orange.fr>
2019-05-10 10:10:25 +0200

    modifs page accueil

--- assets/js/monjs.js
+++ assets/js/monjs.js
@@ -11,13 +11,83 @@ $(document).ready(function(){
   $(".show01").click(function(){
     $(".hide01").toggle();
   });
-  $(".show02").click(function(){
-    $(".hide02").toggle();
+    $(".show02").click(function(){
+  $(".hide02").toggle();
   });
-  $(".show03").click(function(){
-    $(".hide03").toggle();
+    $(".show03").click(function(){
+  $(".hide03").toggle();
   });
-  // $(".show04").click(function(){
-  //   $(".hide04").toggle();
-  // });
+  $(".show04").click(function(){
+    $(".hide04").toggle();
+  });
+  $(".show05").click(function(){
+    $(".hide05").toggle();
+  });  
+  $(".show06").click(function(){
+    $(".hide06").toggle();
+  });  
+  $(".show07").click(function(){
+    $(".hide07").toggle();
+  }); 
+  $(".show08").click(function(){
+    $(".hide08").toggle();
+  });
+  $(".show09").click(function(){
+    $(".hide09").toggle();
+  });
+  $(".show10").click(function(){
+    $(".hide10").toggle();
+  });
+  $(".show11").click(function(){
+    $(".hide11").toggle();
+  });
+  $(".show12").click(function(){
+    $(".hide12").toggle();
+  });
+  $(".show13").click(function(){
+    $(".hide13").toggle();
+  });
+  $(".show14").click(function(){
+    $(".hide14").toggle();
+  });
+  $(".show15").click(function(){
+    $(".hide15").toggle();
+  });
+  $(".show16").click(function(){
+    $(".hide16").toggle();
+  });
+  $(".show17").click(function(){
+    $(".hide17").toggle();
+  });
+  $(".show18").click(function(){
+    $(".hide18").toggle();
+  });
+  $(".show19").click(function(){
+    $(".hide19").toggle();
+  });
+  $(".show20").click(function(){
+    $(".hide20").toggle();
+  });
+  $(".show21").click(function(){
+    $(".hide21").toggle();
+  });
+  $(".show22").click(function(){
+    $(".hide22").toggle();
+  });
+  $(".show23").click(function(){
+    $(".hide23").toggle();
+  });
+  $(".show24").click(function(){
+    $(".hide24").toggle();
+  });
+  $(".show25").click(function(){
+    $(".hide25").toggle();
+  });
+  $(".show26").click(function(){
+    $(".hide26").toggle();
+  });
+  $(".show27").click(function(){
+    $(".hide27").toggle();
+  });
+
 });
